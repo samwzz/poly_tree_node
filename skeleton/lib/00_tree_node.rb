270fdb30cd3d23fbbb4cf11dfced81ab63a605fc
Sam Wang <sam.zz.wang@gmail.com>
2017-03-31 10:47:18 -0700

    Creates PolyTreeNode reader methods

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -2,4 +2,16 @@ class PolyTreeNode
   def initialize(value)
     @value, @parent, @children = value, nil, []
   end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
 end
